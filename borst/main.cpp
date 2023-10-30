c9fd46256995be30cc667e6afa4f88386510b092
Angelo Carly <angelo.carly@protonmail.com>
2023-10-30 08:51:46 +0100

    Add CMakeLists.txt

---
+++ borst/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * main.cpp
+ *
+ * @file	main.cpp
+ * @author	Angelo Carly
+ * @date	30/10/2023
+ *
+ * Copyright (c) 2023 Hybrid Software Development. All rights reserved.
+ */
+
+int main()
+{
+
+}
